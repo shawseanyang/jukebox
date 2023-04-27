c20bfc7b41a1b9ab5aef8d910f937c8e6ccab1f4
Sean Y <seanyang@college.harvard.edu>
2023-04-27 14:02:00 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Landing from "./pages/Landing";
 import Playback from "./pages/Playback";
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {

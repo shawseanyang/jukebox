7e2500b21bfe469a5e89dd83427691b355e76525
Sean Y <seanyang@college.harvard.edu>
2023-04-27 14:34:10 -0400

    Implement landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,13 @@
+import { Button, Row, Divider } from 'antd';
+import { ReactComponent as Jukebox } from '../assets/jukebox.svg';
+
 const Landing = () => {
   return <>
-    <h1>Landing</h1>
+    <Jukebox />
+    <Divider />
+    <Row justify="center">
+        <Button type="default" size="large">Log in with Spotify</Button>
+    </Row>
   </>;
 };
 

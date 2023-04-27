919453789880ddc27c9304b44c57201c548ec22e
Sean Y <seanyang@college.harvard.edu>
2023-04-27 16:34:43 -0400

    Implement queue viewer

--- src/components/QueueViewer.tsx
+++ src/components/QueueViewer.tsx
@@ -1,10 +1,58 @@
-export type QueueViewerProps = {
+import { Avatar, List, Row, Skeleton } from "antd";
+import { Artist, Song } from "../types/Music";
+import Title from "antd/es/typography/Title";
 
+export type QueueViewerProps = {
+  queue: Song[];
+  deleteSong: (index: number) => void;
 };
 
 const QueueViewer = (props: QueueViewerProps) => {
+
+  function formatArtistsNames(artists: Artist[]) {
+    return artists.map((artist) => artist.name).join(", ");
+  }
+
+  function isQueueEmpty() {
+    return props.queue.length === 0;
+  }
+
+  const SkeletonQueue =
+    <List
+      itemLayout="horizontal"
+      dataSource={[1, 2, 3]}
+      renderItem={() => (
+        <List.Item>
+          <Skeleton.Button block />
+        </List.Item>
+      )}
+    />
+
+  const Queue =
+    <List
+      itemLayout="horizontal"
+      dataSource={props.queue}
+      renderItem={(song, index) => (
+        <List.Item
+          actions={[<a key="delete" onClick={() => props.deleteSong(index)}>Delete</a>]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar shape="square" size="large" src={song.album.imageUrl} />}
+            title={song.name}
+            description={formatArtistsNames(song.artists)}
+          />
+        </List.Item>
+      )}
+    />
+
+  // if the queue is empty, render a skeleton. Otherwise, render the queue
   return (
-    <></>
+    <>
+      <Row justify="center">
+        <Title>Up next</Title>
+      </Row>
+      {isQueueEmpty() ? SkeletonQueue : Queue}
+    </>
   )
 }
 
--- src/pages/Playback.tsx
+++ src/pages/Playback.tsx
@@ -5,12 +5,27 @@ import SongAdder from "../components/SongAdder";
 import AlbumCover from "../components/AlbumCover";
 import PlaybackController from "../components/PlaybackController";
 import QueueViewer from "../components/QueueViewer";
+import { Queue } from "../types/Music";
+
+const FAKE_SONG = {
+  name: "Fake Song",
+  artists: [{ name: "Fake Artist 1" }, { name: "Fake Artist 2" }],
+  album: { name: "Fake Album", imageUrl: "https://content-images.p-cdn.com/images/b8/64/67/6b/82be444a8cfadb653356d6a0/_1080W_1080H.jpg" },
+  duration: 300,
+};
+
+const FAKE_QUEUE = [FAKE_SONG, FAKE_SONG, FAKE_SONG];
 
 const Playback = () => {
   const [group, setGroup] = useState<string | null>(null);
+  const [queue, setQueue] = useState<Queue>([]);
 
   function hasJoinedGroup() {
     return group !== null;
+  }
+  
+  function deleteSong(index: number) {
+    // TODO: implement
   }
 
   return (
@@ -29,7 +44,7 @@ const Playback = () => {
       </Row>
       <Row>
         <Col span={12}>
-          <QueueViewer />
+          <QueueViewer queue={FAKE_QUEUE} deleteSong={deleteSong}/>
         </Col>
         <Col span={12}>
           <SongAdder />

c8530b105fc47de1d74587b8637fa5dff085bc03
Sean Y <seanyang@college.harvard.edu>
2023-04-27 23:41:24 -0400

    Add more space vertically

--- src/pages/Playback.tsx
+++ src/pages/Playback.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import JoinGroupModal from "../components/JoinGroupModal";
-import { Col, Row } from "antd";
+import { Col, Row, Space } from "antd";
 import SongAdder from "../components/SongAdder";
 import AlbumCover from "../components/AlbumCover";
 import PlaybackController from "../components/PlaybackController";
@@ -52,32 +52,34 @@ const Playback = () => {
         isOpen={!hasJoinedGroup()}
         joinGroup={setGroup}
       />
-      <Row>
-        <Col span={10}>
-          <AlbumCover imageUrl={currentSong!.album.imageUrl}/>
-        </Col>
-        <Col span={4} />
-        <Col span={10} style={{alignSelf: "end"}}>
-          <PlaybackController
-            song={currentSong}
-            isPlaying={isPlaying}
-            songProgress={songProgress}
-            playSong={playSong}
-            togglePlayback={togglePlayback}
-            skipSong={skipSong}
-            scrubTo={scrubTo}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col span={10}>
-          <QueueViewer queue={FAKE_QUEUE} deleteSong={deleteSong}/>
-        </Col>
-        <Col span={4} />
-        <Col span={10}>
-          <SongAdder addSong={addSong}/>
-        </Col>
-      </Row>
+      <Space direction="vertical" size="large">
+        <Row>
+          <Col span={10}>
+            <AlbumCover imageUrl={currentSong!.album.imageUrl}/>
+          </Col>
+          <Col span={4} />
+          <Col span={10} style={{alignSelf: "end"}}>
+            <PlaybackController
+              song={currentSong}
+              isPlaying={isPlaying}
+              songProgress={songProgress}
+              playSong={playSong}
+              togglePlayback={togglePlayback}
+              skipSong={skipSong}
+              scrubTo={scrubTo}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={10}>
+            <QueueViewer queue={FAKE_QUEUE} deleteSong={deleteSong}/>
+          </Col>
+          <Col span={4} />
+          <Col span={10}>
+            <SongAdder addSong={addSong}/>
+          </Col>
+        </Row>
+      </Space>
     </>
   );
 };

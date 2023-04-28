cbf2106ea4ecc4974d854df9c6266bfec6ede640
Sean Y <seanyang@college.harvard.edu>
2023-04-27 20:09:55 -0400

    Change font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import Layout from "./components/Layout";
 import Landing from "./pages/Landing";
 import Playback from "./pages/Playback";
 import './App.css';
+import { ConfigProvider } from "antd";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Landing />} />
-          <Route path="/playback" element={<Playback />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "Inter",
+          fontSizeHeading1: 25,
+          fontSizeHeading2: 20,
+          fontSizeHeading3: 16,
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Landing />} />
+            <Route path="/playback" element={<Playback />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
